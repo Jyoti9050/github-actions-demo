86bf520bae74a3b982bff39ce8d708f937696a2b
Jyoti9050 <153624167+Jyoti9050@users.noreply.github.com>
2025-06-20 17:04:53 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,17 @@
-output "ec2_instance_public_ip" {
-  value = aws_instance.cloudndevops_pipeline_ec2.public_ip
+output "s3_bucket_name" {
+  description = "Name of s3 Bucket" 
+  value       = aws_s3_bucket.cloudndevops.id
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.cloudndevops.id
+output "ec2_instance_public_ip" {
+  description = "The public IP address of the ec2instance"
+  value       = aws_instance.cloudndevops_pipeline_ec2.public_ip
 }
+
+output "ec2_instance_url" {
+  description = "The url to access the Nginx Server"
+  value       = "http://${aws_instance.cloudndevops_pipeline_ec2.public_ip}"
+}
+
+
+
