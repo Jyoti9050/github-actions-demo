8bc6eff3515182183f8f9eed621736fe08c2e2cb
Jyoti9050 <153624167+Jyoti9050@users.noreply.github.com>
2025-06-20 15:26:42 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "cloudndevops-terraform-tfstate"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+  #backend "s3" {
+   # bucket = "cloudndevops-terraform-tfstate"
+   # key    = "terraform.tfstate"
+   # region = "us-east-1"
+ # }
+#}
