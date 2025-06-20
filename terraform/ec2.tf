c8040a567940b03af602c33daaf0e37ce4685782
Jyoti9050 <153624167+Jyoti9050@users.noreply.github.com>
2025-06-20 16:29:22 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "cloudndevops_pipeline_ec2" {
+  ami                         = "ami-09e6f87a47903347c"    # Update to latest AMI ID for your region
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+                              #!/bin/bash
+                              sudo yum install nginx -y
+                              sudo systemctl start nginx
+                              EOF
+  tags = {
+    Name = "GitHubActionsEC2"
+  }
+}
