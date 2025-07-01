62ff17451929e6fae5caae1abec83fa6954ac940
Jyoti9050 <153624167+Jyoti9050@users.noreply.github.com>
2025-07-01 15:40:40 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "cloudndevops_pipeline_ec2" {
-  ami                         = "ami-09e6f87a47903347c"    # Update to latest AMI ID for your region
+  ami                         = "ami-05ffe3c48a9991133"    # Update to latest AMI ID for your region
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public-subnet.id
   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
